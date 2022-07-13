7c3a3675266fe9a38193dfad2c10442f19b90fa4
Ro <ro@openform.co>
2022-07-13 12:23:15 -0700

    scroll animation

--- scripts.js
+++ scripts.js
@@ -1,14 +1,49 @@
 const menuManager = () => {
-	var atTop = true;
+	let atTop = (window.scrollY <= 24);
+	const topThreshold = 24;
 	const menuBar = document.querySelector('.menuBar');
 
-	document.addEventListener('scroll', () => {
+	// on Load
+	if (atTop) {
+		requestAnimationFrame(() => menuBar.classList.add('keepOpen', 'instant'));
+		setTimeout(() => requestAnimationFrame(() => menuBar.classList.remove('instant')), 1);
+	}
+
+	const scrollHandler = () => {
 		const scrollY = window.scrollY;
-		if ((scrollY > 4 && atTop) || (scrollY <= 4 && !atTop)) {
+		if ((scrollY > topThreshold && atTop) || (scrollY <= topThreshold && !atTop)) {
 			atTop = !atTop;
 			requestAnimationFrame(() => menuBar.classList.toggle('keepOpen'));
 		}
+	}
+
+	document.addEventListener('scroll', scrollHandler, {passive: true});
+}
+
+const colorCoding = () => {
+	const navItems = document.querySelectorAll('.menuBar a');
+	console.log(navItems);
+	const colorItems = document.querySelectorAll('.colorSync');
+	console.log(colorItems);
+
+	const pointerAdd = (color) => () => {
+		colorItems.forEach(item =>
+			requestAnimationFrame(() => item.classList.add(`cs-${color}`)));
+	}
+	const pointerRemove = (color) => () => {
+		colorItems.forEach(item =>
+			requestAnimationFrame(() => item.classList.remove(`cs-${color}`)));
+	}
+
+	navItems.forEach(item => {
+		item.addEventListener('pointerenter', pointerAdd(item.attributes.dataColor.nodeValue));
+		item.addEventListener('pointerout', pointerRemove(item.attributes.dataColor.nodeValue));
 	})
 }
 
-window.addEventListener('DOMContentLoaded', menuManager);
+const loaded = () => {
+	menuManager();
+	colorCoding();
+}
+
+document.addEventListener('DOMContentLoaded', loaded);
